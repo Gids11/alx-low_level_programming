89b689fedc0535128d73f7995665ab293b59727b
Gideon <gitom127@gmail.com>
2023-03-17 22:33:09 +0000

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - main entry for alphabets
+ *
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar("i\n");
+	return (0);
+}
