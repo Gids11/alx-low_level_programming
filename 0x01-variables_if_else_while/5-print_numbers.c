f2256044d11051aa49cf779741ca8b131641b32d
Gideon <gitom127@gmail.com>
2023-03-20 21:40:18 +0000

    single digit numbers including 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ ** main - single digit numbers of base 10 starting from 0.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int numb;
+
+for (numb = 0; numb < 10; numb++)
+printf("%d", numb);
+printf("\n");
+return (0);
+}
