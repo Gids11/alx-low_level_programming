0a3b9ce196f31710f95c94d7f5b23f3456459e58
Gideon Tommy Joshua <gidtom127@gmail.com>
2023-04-17 07:45:21 -0700

    printing current input file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this program prints the current input file
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Name of current input file is: %s\n", __FILE__);
+	return (0);
+}
