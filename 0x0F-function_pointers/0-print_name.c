35b85182f55f8fffb2b265cbb1e5ec00e3e02ae4
Gideon Tommy Joshua <gidtom127@gmail.com>
2023-04-19 13:27:37 -0700

    executing a function as an array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - a function that prints a name
  * @name: the name
- * @f: a pointer to the function 
+ * @f: a pointer to the function
  */
 void print_name(char *name, void (*f)(char *))
 {
