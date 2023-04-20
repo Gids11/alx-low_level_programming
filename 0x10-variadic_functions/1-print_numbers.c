af5156c2b29488ef0ef1d13c76e7413a7fd08aed
Gideon Tommy Joshua <gidtom127@gmail.com>
2023-04-20 13:44:54 -0700

    printing numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - printing numbers with a separator
+ * @n: the numbers
+ * @separator: the separator
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list pr;
+		unsigned int i;
+
+		va_start(pr, n);
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(pr, unsigned int));
+			if (i != (n - 1) && separator != NULL)
+					printf("%s", separator);
+					}
+					printf("\n");
+					va_end(pr);
+					}
