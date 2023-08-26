57ec5d41d4b1b3b9f818bec75f4d7f731696d19d
Gideon <gidtom127@gmail.com>
2023-08-25 17:58:15 -0700

    creating file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  * create_file - this function creates a file.
  * @filename: The name of file to create.
- * @text_content: a string 
+ * @text_content: a string
  * Return: 1 on success, -1 otherwise
  */
 int create_file(const char *filename, char *text_content)
